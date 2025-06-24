4d7e373d88892a4c8c1f681356de1ff8d83b77a4
georgII-devOP <75487851+georgII-devOP@users.noreply.github.com>
2025-06-24 18:36:16 +0300

    Дополнительные тесты

---
+++ FoodServiceInventoryApp/FoodServiceInventoryApp.Tests/InverseBooleanToVisibilityConverterTests.cs
@@ -0,0 +1,91 @@
+﻿using Xunit;
+using FoodServiceInventoryApp.Converters;
+using System.Windows;
+using System;
+using System.Globalization;
+
+namespace FoodServiceInventoryApp.Tests
+{
+    public class InverseBooleanToVisibilityConverterTests
+    {
+        private readonly InverseBooleanToVisibilityConverter _converter;
+
+        public InverseBooleanToVisibilityConverterTests()
+        {
+            _converter = new InverseBooleanToVisibilityConverter();
+        }
+
+
+        [Fact]
+        public void Convert_True_ReturnsCollapsed()
+        {
+            var result = _converter.Convert(true, typeof(Visibility), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(Visibility.Collapsed, result);
+        }
+
+        [Fact]
+        public void Convert_False_ReturnsVisible()
+        {
+            var result = _converter.Convert(false, typeof(Visibility), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(Visibility.Visible, result);
+        }
+
+        [Fact]
+        public void Convert_NonBooleanValue_ReturnsCollapsed()
+        {
+            var result = _converter.Convert("some string", typeof(Visibility), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(Visibility.Collapsed, result);
+        }
+
+        [Fact]
+        public void Convert_Null_ReturnsCollapsed()
+        {
+            var result = _converter.Convert(null, typeof(Visibility), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(Visibility.Collapsed, result);
+        }
+
+        [Fact]
+        public void ConvertBack_Collapsed_ReturnsTrue()
+        {
+            var result = _converter.ConvertBack(Visibility.Collapsed, typeof(bool), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(true, result);
+        }
+
+        [Fact]
+        public void ConvertBack_Visible_ReturnsFalse()
+        {
+            var result = _converter.ConvertBack(Visibility.Visible, typeof(bool), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(false, result);
+        }
+
+        [Fact]
+        public void ConvertBack_Hidden_ReturnsFalse()
+        {
+            var result = _converter.ConvertBack(Visibility.Hidden, typeof(bool), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(false, result);
+        }
+
+        [Fact]
+        public void ConvertBack_NonVisibilityValue_ReturnsFalse()
+        {
+            var result = _converter.ConvertBack("some string", typeof(bool), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(false, result);
+        }
+
+        [Fact]
+        public void ConvertBack_Null_ReturnsFalse()
+        {
+            var result = _converter.ConvertBack(null, typeof(bool), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(false, result);
+        }
+    }
+}
---
+++ FoodServiceInventoryApp/FoodServiceInventoryApp.Tests/StringToVisibilityConverterTests.cs
@@ -0,0 +1,78 @@
+﻿using Xunit;
+using FoodServiceInventoryApp.Converters;
+using System.Windows;
+using System;
+using System.Globalization;
+
+namespace FoodServiceInventoryApp.Tests
+{
+    public class StringToVisibilityConverterTests
+    {
+        private readonly StringToVisibilityConverter _converter;
+
+        public StringToVisibilityConverterTests()
+        {
+            _converter = new StringToVisibilityConverter();
+        }
+
+
+        [Fact]
+        public void Convert_NonNullOrWhitespaceString_ReturnsVisible()
+        {
+            string value = "Hello World";
+
+            var result = _converter.Convert(value, typeof(Visibility), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(Visibility.Visible, result);
+        }
+
+        [Fact]
+        public void Convert_EmptyString_ReturnsCollapsed()
+        {
+            string value = "";
+
+            var result = _converter.Convert(value, typeof(Visibility), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(Visibility.Collapsed, result);
+        }
+
+        [Fact]
+        public void Convert_NullString_ReturnsCollapsed()
+        {
+            string value = null;
+
+            var result = _converter.Convert(value, typeof(Visibility), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(Visibility.Collapsed, result);
+        }
+
+        [Fact]
+        public void Convert_WhitespaceString_ReturnsCollapsed()
+        {
+            string value = "   \t\n ";
+
+            var result = _converter.Convert(value, typeof(Visibility), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(Visibility.Collapsed, result);
+        }
+
+        [Fact]
+        public void Convert_NonStringObject_ReturnsVisible()
+        {
+            object value = 123;
+
+            var result = _converter.Convert(value, typeof(Visibility), null, CultureInfo.InvariantCulture);
+
+            Assert.Equal(Visibility.Collapsed, result);
+        }
+
+        [Fact]
+        public void ConvertBack_ThrowsNotImplementedException()
+        {
+            Assert.Throws<NotImplementedException>(() =>
+            {
+                _converter.ConvertBack(Visibility.Visible, typeof(string), null, CultureInfo.InvariantCulture);
+            });
+        }
+    }
+}
